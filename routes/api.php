2088d7f71551df65bdb5ad83b482a3f5fe566ebc
Harpreet Singh <singh.harpreet89@hotmail.com>
2020-05-18 04:35:42 -0400

    Created the required models

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{productId}/reviews', 'ReviewController');
 });
+
